2b82ad8a8914e90d4ee323d6dc2d5b82a03507d0
elir0d <rodrigomacade@outlook.com>
2021-09-02 12:40:05 -0300

    feat: :truck: Add routes to the tabs footer
    Add route path to the main route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'inicio', loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule) },
-  { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule) }
-
+  { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule) },
+  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) }
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
