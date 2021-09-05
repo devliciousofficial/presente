a4beca3facec47b06d1bd0eecefbf340952c5af4
elir0d <rodrigomacade@outlook.com>
2021-09-05 18:46:57 -0300

    chore(folders): create pages folder
    add a new foder the will wrap all application pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) },
-  { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule) },
-  { path: 'cadastro', loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule) },
-  { path: 'alterar-senha', loadChildren: () => import('./alterar-senha/alterar-senha.module').then( m => m.AlterarSenhaPageModule) },
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule) },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule) },
+  { path: 'cadastro', loadChildren: () => import('./pages/cadastro/cadastro.module').then( m => m.CadastroPageModule) },
+  { path: 'alterar-senha', loadChildren: () => import('./pages/alterar-senha/alterar-senha.module').then( m => m.AlterarSenhaPageModule) },
 ];
 @NgModule({
   imports: [
@@ -14,4 +14,5 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule {}
