65892f741afd8aa01717a4bb2f0b82fe1119f841
elir0d <49011043+elir0d@users.noreply.github.com>
2021-09-20 11:48:55 -0300

    revert(remove parse init from main component): remove parse init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import Parse from 'parse'
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
-   Parse.initialize('3auUTevR9wvgsYWt0BqYoQUHQV8Uz634k48WGhIk','aB1zPk1OjreFfXAzU1ECz86lJ0u8wqiQPcMpVE2D');
-   Parse.serverURL = 'https://parseapi.back4app.com';
-  }
+  constructor() {}
 }
