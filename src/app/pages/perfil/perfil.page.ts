70d94de6398e72a164d2812e83a335f92575d54b
elir0d <49011043+elir0d@users.noreply.github.com>
2021-09-23 21:19:21 -0300

    feat(add logout feature): add logout feature

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Parse from 'parse';
 
 @Component({
-  selector: 'app-perfil',
-  templateUrl: './perfil.page.html',
-  styleUrls: ['./perfil.page.scss'],
+   selector: 'app-perfil',
+   templateUrl: './perfil.page.html',
+   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
 
-  constructor(private router: Router) { }
+   constructor(private router: Router) {
+      //TODO - guardar ID do app e Chave JS na pasta environment
+      Parse.initialize('3auUTevR9wvgsYWt0BqYoQUHQV8Uz634k48WGhIk', 'aB1zPk1OjreFfXAzU1ECz86lJ0u8wqiQPcMpVE2D');
+      Parse.serverURL = 'https://parseapi.back4app.com';
+   }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
-  sair(){ this.router.navigate(['inscricao']); }
+   sair() {
+      Parse.User.logOut();
+      console.log('Logout user');
+      this.router.navigate(['inscricao']);
+   }
 
 }
