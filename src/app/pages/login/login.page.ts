c8611f0590c543f77ebc6960d812b757d37f0a9b
elir0d <49011043+elir0d@users.noreply.github.com>
2021-09-23 21:21:47 -0300

    refactor(login): remove smell code

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,20 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginPageForm } from './login.page.form';
+import Parse from 'parse';
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+   selector: 'app-login',
+   templateUrl: './login.page.html',
+   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  form: FormGroup;
+   form: FormGroup;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) { }
+   username: string;
+   email: string;
+   password: string;
 
-  ngOnInit() { this.form = new LoginPageForm(this.formBuilder).createForm(); }
-  login(){ this.router.navigate(['tabs']); }
-  cadastro(){ this.router.navigate(['cadastro']); }
-  
+   constructor(private router: Router, private formBuilder: FormBuilder) {
+      //TODO - guardar ID do app e Chave JS na pasta environment
+      Parse.initialize('3auUTevR9wvgsYWt0BqYoQUHQV8Uz634k48WGhIk', 'aB1zPk1OjreFfXAzU1ECz86lJ0u8wqiQPcMpVE2D');
+      Parse.serverURL = 'https://parseapi.back4app.com';
+   }
+
+   ngOnInit() { this.form = new LoginPageForm(this.formBuilder).createForm(); }
+   loginTeste() { this.router.navigate(['tabs']); }
+   cadastro() { this.router.navigate(['cadastro']); }
+
+   // eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
+   login() {
+      (async () => {
+         try {
+            // Pass the username and password to logIn function
+            const user = await Parse.User.logIn(this.email, this.password);
+            // Do stuff after successful login
+            console.log('Logged in user', user);
+            this.router.navigate(['tabs']);
+         } catch (error) {
+            console.error('Error while logging in user', error);
+         }
+      })();
+   }
 }
