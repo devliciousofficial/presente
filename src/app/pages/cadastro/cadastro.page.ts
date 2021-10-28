ac488bbf8cc7371d669a3a9abeb9ba813bd5ad43
elir0d <49011043+elir0d@users.noreply.github.com>
2021-10-28 17:01:33 -0300

    feat(erros): creates error handling
    Add new module and functions that will be responsible for error handling when do login or signup

--- src/app/pages/cadastro/cadastro.page.ts
+++ src/app/pages/cadastro/cadastro.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 /* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -6,6 +7,7 @@ import { CadastroPageForm } from './cadastro.page.form';
 
 import Parse from 'parse';
 import { environment } from '../../../environments/environment';
+import { AlertController } from '@ionic/angular';
 
 //TODO - criar uma rota segura por meio de guards após criar conta e tentar fazer login//
 
@@ -18,21 +20,57 @@ export class CadastroPage implements OnInit {
 
    form: FormGroup;
 
-   name: string;
-   username: string;
-   email: string;
-   password: string;
-   confirmPassword: string;
+   name: string = '';
+   username: string = '';
+   email: string = '';
+   password: string = '';
+   confirmPassword: string = '';
 
-   constructor(private router: Router, private formBuilder: FormBuilder) {
+   constructor(private router: Router, private formBuilder: FormBuilder, private alertController: AlertController) {
       Parse.initialize(environment.APP_ID, environment.JS_KEY);
       Parse.serverURL = 'https://parseapi.back4app.com';
    }
 
    ngOnInit() { this.form = new CadastroPageForm(this.formBuilder).createForm(); }
 
-   cadastroComFacebook() { this.router.navigate(['tabs']);};
-   cadastroComTwitter() { this.router.navigate(['tabs']);};
+
+   // TODO: Criar um componente para lidar com os erros de login //
+   async exibirAlertaSenha() {
+      const meuAlerta = await this.alertController.create({
+         cssClass: 'my-custom-class',
+         header: 'Erro ao criar cadastro!',
+         message: 'As senhas não coincidem. Tente novamente',
+         buttons: ['OK']
+      });
+
+      await meuAlerta.present();
+   }
+
+   async exibirAlertaUsuario() {
+      const meuAlerta = await this.alertController.create({
+         cssClass: 'my-custom-class',
+         header: 'Erro ao criar cadastro!',
+         message: 'Usuário já existe. Escolha outro email e tente novamente.',
+         buttons: ['OK']
+      });
+
+      await meuAlerta.present();
+   }
+
+   async exibirAlertaCamposObrigatorios() {
+      const meuAlerta = await this.alertController.create({
+         cssClass: 'my-custom-class',
+         header: 'Erro ao criar cadastro!',
+         message: 'Não foi possível criar cadastro. Preencha os campos obrigatórios',
+         buttons: ['OK']
+      });
+
+      await meuAlerta.present();
+   }
+
+   //TODO: Implemntar o cadastro com as tedes sociais - Funcoes apenas com rotas para a tela principal do app//
+   cadastroComFacebook() { this.router.navigate(['tabs']); };
+   cadastroComTwitter() { this.router.navigate(['tabs']); };
 
    cadastrar() {
       (async () => {
@@ -46,16 +84,29 @@ export class CadastroPage implements OnInit {
             if (this.password !== this.confirmPassword) {
                this.password = '';
                this.confirmPassword = '';
+               this.exibirAlertaSenha();
                throw new Error('As senhas não coincidem. Tente novamente');
             }
 
             const userResult = await user.signUp();
             Parse.User.logOut();
-            this.router.navigate(['login']);
+            this.router.navigate(['email-enviado']);
             console.log('User signed up. Please verify your e-mail');
 
          } catch (error) {
             console.error('Error while signing up user', error);
+            if (error.code === 202){
+               this.exibirAlertaUsuario();
+               this.name = '';
+               this.username = '';
+               this.password = '';
+               this.confirmPassword = '';
+            }
+
+            else if (!this.username || !this.username.length || error.code === 142) {
+               this.exibirAlertaCamposObrigatorios();
+               throw new Error('Não foi possível criar cadastro. Preencha os campos obrigatórios');
+             }
          }
       })();
    }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -5,6 +6,7 @@ import { LoginPageForm } from './login.page.form';
 
 import Parse from 'parse';
 import { environment } from '../../../environments/environment';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -16,18 +18,54 @@ export class LoginPage implements OnInit {
 
    form: FormGroup;
 
-   username: string;
-   email: string;
-   password: string;
+   username: string = '';
+   email: string = '';
+   password: string = '';
 
-   constructor(private router: Router, private formBuilder: FormBuilder) {
+   constructor(private router: Router, private formBuilder: FormBuilder, private alertController: AlertController) {
       Parse.initialize(environment.APP_ID, environment.JS_KEY);
       Parse.serverURL = 'https://parseapi.back4app.com';
    }
 
    ngOnInit() { this.form = new LoginPageForm(this.formBuilder).createForm(); }
-   loginTeste() { this.router.navigate(['tabs']); }
-   redefinirSenha(){ this.router.navigate(['redefinir']); }
+
+   //TODO: Criar um componente para lidar com todos os erros abaixo//
+   async exibirAlertaUsuarioOuSenhaInvalido() {
+      const meuAlerta = await this.alertController.create({
+         cssClass: 'my-custom-class',
+         header: 'Erro ao tentar fazer Login!',
+         message: 'Usuario ou Senha Inválido.',
+         buttons: ['OK']
+      });
+
+      await meuAlerta.present();
+   }
+
+   async exibirAlertaEmailNaoVerificado() {
+      const meuAlerta = await this.alertController.create({
+         cssClass: 'my-custom-class',
+         header: 'Conta não verificada!',
+         message: 'Verifique sua conta atráves do link enviado por email.',
+         buttons: ['OK']
+      });
+
+      await meuAlerta.present();
+   }
+
+   async exibirAlertaUsuarioSenhaObrigatorios() {
+      const meuAlerta = await this.alertController.create({
+         cssClass: 'my-custom-class',
+         header: 'Erro ao tentar fazer Login!',
+         message: 'Verifique se todos os campos foram preenchidos.',
+         buttons: ['OK']
+      });
+
+      await meuAlerta.present();
+   }
+
+    //TODO: Implemntar o login com as tedes sociais - Funcoes apenas com rotas para a tela principal do app//
+    loginComFacebook() { this.router.navigate(['tabs']); };
+    loginComTwitter() { this.router.navigate(['tabs']); };
 
    // eslint-disable-next-line @typescript-eslint/adjacent-overload-signatures
    login() {
@@ -39,9 +77,26 @@ export class LoginPage implements OnInit {
             // Do stuff after successful login
             console.log('Logged in user', user);
             this.router.navigate(['tabs']);
+
          } catch (error) {
             console.error('Error while logging in user', error);
+
+            if (error.code === 101) {
+               this.username = '';
+               this.password = '';
+               this.exibirAlertaUsuarioOuSenhaInvalido();
+            }
+
+            else if (error.code === 205) {
+               this.exibirAlertaEmailNaoVerificado();
+            }
+
+            else if (error.code === 200 || error.code === 201) {
+               this.exibirAlertaUsuarioSenhaObrigatorios();
+            }
          }
+
+
       })();
    }
 }
