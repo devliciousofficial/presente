89329d1f77967dfabb97593856b0e4e10199e6ed
elir0d <49011043+elir0d@users.noreply.github.com>
2021-11-19 00:56:35 -0300

    feat(inicio): add product cards
    create a infinite scroll and add product cards

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 @Component({
   selector: 'app-inicio',
   templateUrl: 'inicio.page.html',
   styleUrls: ['inicio.page.scss']
 })
 export class InicioPage {
+   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  constructor() {}
+   image = 'https://parsefiles.back4app.com/3auUTevR9wvgsYWt0BqYoQUHQV8Uz634k48WGhIk/52f81bd8776f4023805fefc18b7835ff_logo1.jpg';
+   lojas: any = [];
+   lojasPage: any = [];
 
-}
+   private readonly offset: number = 12;
+   private index = 0;
+
+   constructor() {
+      // dummie data //
+      for (let i = 0; i < 50; i++) {
+         this.lojas.push(`Item ${i + 1}`);
+      }
+
+      this.lojasPage = this.lojas.slice(this.index, this.offset + this.index);
+      this.index += this.offset;
+
+   }
+
+   loadData(event) {
+     setTimeout(() => {
+       const lojas = this.lojas.slice(this.index, this.offset + this.index);
+       this.index += this.offset;
+
+       // eslint-disable-next-line @typescript-eslint/prefer-for-of
+       for (let i = 0; i < lojas.length; i++) {
+         this.lojasPage.push(lojas[i]);
+      }
+
+      event.target.complete();
+
+       // App logic to determine if all data is loaded
+       // and disable the infinite scroll
+      //  const data = '1234567891011';
+       if (this.lojasPage.length === 50) {
+         event.target.disabled = true;
+       }
+     }, 500);
+   }
+ }
+
+
